4951c56f923090f590c22fbcb8b996159bc7542a
XuJ <xujin@startimes.com.cn>
2025-08-26 09:38:31 +0800

    接雨水

--- src/main/java/com/xj/leetcode/leetcode/editor/cn/TrappingRainWater.java
+++ src/main/java/com/xj/leetcode/leetcode/editor/cn/TrappingRainWater.java
@@ -8,12 +8,24 @@ public class TrappingRainWater {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int trap(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
 
-
-
-
-
-            return 0;
+            int preSum = 0;
+            int sufSum = 0;
+            int sum = 0;
+            while (left <= right){
+                preSum = Math.max(preSum, height[left]);
+                sufSum = Math.max(sufSum, height[right]);
+                if (preSum < sufSum){
+                    sum += (preSum - height[left]);
+                    left++;
+                }else{
+                    sum += (sufSum - height[right]);
+                    right--;
+                }
+            }
+            return sum;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
