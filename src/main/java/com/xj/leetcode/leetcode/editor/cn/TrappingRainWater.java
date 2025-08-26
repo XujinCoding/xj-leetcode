b8aef19ff637d2fa297db24c6bc9624c7215d76e
XuJ <xujin@startimes.com.cn>
2025-08-26 09:30:27 +0800

    初始化

---
+++ src/main/java/com/xj/leetcode/leetcode/editor/cn/TrappingRainWater.java
@@ -0,0 +1,27 @@
+package com.xj.leetcode.leetcode.editor.cn;
+
+import java.util.*;
+import com.xj.leetcode.common.*;
+
+public class TrappingRainWater {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int trap(int[] height) {
+
+
+
+
+
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new TrappingRainWater().new Solution();
+        // put your test code here
+        
+    }
+}
