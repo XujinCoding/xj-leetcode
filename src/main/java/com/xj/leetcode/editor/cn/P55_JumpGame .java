87ba97443aa6334b080f6a91a235fcdd7e75aaf5
XuJ <xujin@startimes.com.cn>
2025-09-05 10:37:36 +0800

    55 跳跃游戏

---
+++ src/main/java/com/xj/leetcode/editor/cn/P55_JumpGame .java
@@ -0,0 +1,91 @@
+//给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。 
+//
+// 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,3,1,1,4]
+//输出：true
+//解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1,0,4]
+//输出：false
+//解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// 0 <= nums[i] <= 10⁵ 
+// 
+//
+// Related Topics 贪心 数组 动态规划 👍 3126 👎 0
+
+
+package com.xj.leetcode.editor.cn;
+
+/**
+ * 跳跃游戏
+ *
+ * @author XuJ
+ * @date 2025-09-04 16:40:31
+ */
+class P55_JumpGame {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P55_JumpGame().new Solution();
+        System.out.println(solution.canJump(new int[]{3,2,1,0,4}));
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean canJump(int[] nums) {
+            //最远可以到达的位置
+            int mx = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > mx) { // 无法到达 i
+                    return false;
+                }
+                mx = Math.max(mx, i + nums[i]); // 从 i 最右可以跳到 i + nums[i]
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    class SolutionTest {
+        public boolean canJump(int[] nums) {
+            boolean[] dp = new boolean[nums.length];
+            int index = nums.length-1;
+            dp[index] = true;
+            for (int i = index-1; i >=0; i--) {
+                int num = nums[i];
+                if (num == 0){
+                    continue;
+                }
+                int left = i+1;
+                int right = Math.min(i+num , nums.length-1);
+                for (int j = left; j <=right; j++) {
+                    if (dp[j]){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[0];
+        }
+
+    }
+
+}
