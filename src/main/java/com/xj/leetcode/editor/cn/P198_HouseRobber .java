c992aa1fb630afb53cbb306706c33d165e4748d4
XuJ <xujin@startimes.com.cn>
2025-09-15 11:33:41 +0800

    198 打家劫舍

---
+++ src/main/java/com/xj/leetcode/editor/cn/P198_HouseRobber .java
@@ -0,0 +1,82 @@
+//你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上
+//被小偷闯入，系统会自动报警。 
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[1,2,3,1]
+//输出：4
+//解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+//     偷窃到的最高金额 = 1 + 3 = 4 。 
+//
+// 示例 2： 
+//
+// 
+//输入：[2,7,9,3,1]
+//输出：12
+//解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+//     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 400 
+// 
+//
+// Related Topics 数组 动态规划 👍 3352 👎 0
+
+
+package com.xj.leetcode.editor.cn;
+
+import com.xj.leetcode.common.*;
+
+/**
+ * 打家劫舍
+ *
+ * @author XuJ
+ * @date 2025-09-15 10:50:39
+ */
+class P198_HouseRobber {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P198_HouseRobber().new Solution();
+		solution.rob(new int[]{2,1,1,5,1,1,2});
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int rob(int[] nums) {
+            //dp[i]表示前 i-2 间房屋能偷窃到的最高总金额
+            int[] dp = new int[nums.length + 2];
+            dp[2] = nums[0];
+            for (int i = 3; i < dp.length; i++) {
+                dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i-1]);
+            }
+            return dp[dp.length-1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+class SolutionTest {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 2];
+        int res = nums[0];
+        dp[2] = nums[0];
+        for (int i = 3; i < dp.length; i++) {
+            int max1 = dp[i - 2] + nums[i - 2];
+            int max2 = res + nums[i - 2] - nums[i - 3];
+            dp[i] = Math.max(max1, max2);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+}
