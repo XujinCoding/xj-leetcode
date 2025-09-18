513ab22241e892e62a097513e215b823d88f03e8
XuJ <xujin@startimes.com.cn>
2025-09-18 10:15:37 +0800

    240 搜索二维矩阵2

---
+++ src/main/java/com/xj/leetcode/editor/cn/P240_SearchA2dMatrixIi .java
@@ -0,0 +1,93 @@
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性： 
+//
+// 
+// 每行的元素从左到右升序排列。 
+// 每列的元素从上到下升序排列。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 5
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21
+//,23,26,30]], target = 20
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= n, m <= 300 
+// -10⁹ <= matrix[i][j] <= 10⁹ 
+// 每行的所有元素从左到右升序排列 
+// 每列的所有元素从上到下升序排列 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 二分查找 分治 矩阵 👍 1718 👎 0
+
+
+package com.xj.leetcode.editor.cn;
+
+import com.xj.leetcode.common.*;
+
+/**
+ * 搜索二维矩阵 II
+ *
+ * @author XuJ
+ * @date 2025-09-17 09:09:51
+ */
+class P240_SearchA2dMatrixIi {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P240_SearchA2dMatrixIi().new Solution();
+        solution.searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5);
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int i = 0;
+            int j = matrix[0].length - 1;
+            while (j >= 0 && i < matrix.length) {
+                if (target > matrix[i][j]) {
+                    i++;
+                } else if (target < matrix[i][j]) {
+                    j--;
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class SolutionTest {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int i = matrix.length - 1, j = 0;
+            while (i >= 0 && j < matrix[0].length) {
+                if (matrix[i][j] > target) i--;
+                else if (matrix[i][j] < target) j++;
+                else return true;
+            }
+            return false;
+        }
+    }
+
+
+}
