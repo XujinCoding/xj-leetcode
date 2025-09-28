c5dc8ce277fd01576e95aefa89736e4ecad992f0
XuJ <xujin@startimes.com.cn>
2025-09-28 16:49:10 +0800

    461 汉明距离

---
+++ src/main/java/com/xj/leetcode/editor/cn/P461_HammingDistance .java
@@ -0,0 +1,75 @@
+//两个整数之间的 汉明距离 指的是这两个数字对应二进制位不同的位置的数目。 
+//
+// 给你两个整数 x 和 y，计算并返回它们之间的汉明距离。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 1, y = 4
+//输出：2
+//解释：
+//1   (0 0 0 1)
+//4   (0 1 0 0)
+//       ↑   ↑
+//上面的箭头指出了对应二进制位不同的位置。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = 3, y = 1
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= x, y <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 注意：本题与 2220. 转换数字的最少位翻转次数 相同。 
+//
+// Related Topics 位运算 👍 769 👎 0
+
+
+package com.xj.leetcode.editor.cn;
+import com.xj.leetcode.common.*;
+
+/**
+ * 汉明距离
+ * @author XuJ
+ * @date 2025-09-28 16:34:46
+ */
+class P461_HammingDistance{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P461_HammingDistance().new Solution();
+		 System.out.println(solution.hammingDistance(1, 3));
+	 }
+
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int hammingDistance(int x, int y) {
+		int res = 0;
+        while(x!=0 || y!=0){
+			int xp = x%2;
+			int yp = y%2;
+			x = x/2;
+			y = y/2;
+			if (xp!=yp){
+				res++;
+			}
+		}
+		return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
